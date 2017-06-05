78c902ad9a8d67bd9edff2c0bb2668f447559807
Thomas Nguyen <tnguy162@calpoly.edu>
2017-06-05 16:02:49 -0700

    Better solution without touching component class

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -6,11 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username = "";
   
-  onClearUsername() {
-    this.username = "";
-  }
+  // Defining this is not necessary if we do inline model assignment in the TS file.
+  // Although both solutions will work.
+  // username = "";
+  // 
+  // onClearUsername() {
+  //   this.username = "";
+  // }
 }
 
 
