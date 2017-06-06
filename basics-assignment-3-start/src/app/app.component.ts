cb70d055892a645e3c7838425e99e212142bf0af
Thomas Nguyen <tnguy162@calpoly.edu>
2017-06-06 12:07:00 -0700

    Finished assign 3

---
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  displayDetails = true;
+  buttonClickLogs = [];
+  
+  onDisplayDetails() {
+    this.displayDetails = !this.displayDetails;
+    this.buttonClickLogs.push(this.buttonClickLogs.length);
+  }
+}
